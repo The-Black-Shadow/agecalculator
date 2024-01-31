04fa8edbca0b2ae5c5d62d0496234f3ba4a52e62
Md Mehedi Hasan <mehedihasan.cse18@gmail.com>
2024-01-31 11:29:32 +0600

    added admob key

--- lib/components/ad_helper.dart
+++ lib/components/ad_helper.dart
@@ -6,11 +6,9 @@ import 'package:google_mobile_ads/google_mobile_ads.dart';
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
-    } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
+      return 'ca-app-pub-7040228706164271/2255687112';
     } else {
-      throw new UnsupportedError('Unsupported platform');
+      throw UnsupportedError('Unsupported platform');
     }
   }
 
